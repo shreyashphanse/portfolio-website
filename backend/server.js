44dd4f35abca45b447794de6bae370fefc04e931
shreyash-demo <shreyashphanse22@gmail.com>
2026-03-07 11:24:19 +0530

    update in server file

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/posts", postRoutes);
 app.use("/api/site", siteRoutes);
-app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 connectDB();
 
